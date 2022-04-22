9e2fbf4a71eba8730221d0f02705382fdce40ee5
Artur Campos <arturcamposba@gmail.com>
2022-04-22 13:54:32 -0300

    Done

--- src/pages/ListItems.js
+++ src/pages/ListItems.js
@@ -1,81 +1,100 @@
 import {
-  IonButton,
   IonCardTitle,
-  IonCol,
   IonContent,
-  IonFooter,
   IonGrid,
   IonHeader,
   IonPage,
-  IonRouterLink,
-  IonRow
+  IonRow,
+  IonList,
+  IonImg,
+  IonToolbar,
+  IonCol,
+  IonButtons,
+  IonBackButton,
+  IonButton,
+  IonIcon
 } from "@ionic/react";
+import { arrowBack, shapesOutline } from "ionicons/icons";
+
 import { useEffect, useState } from "react";
-import { Action } from "../components/Action";
 import styles from "./ListItems.module.scss";
 
 const ListItems = () => {
   const [campeonato, setCampeonato] = useState([]);
 
   useEffect(() => {
-    fetch("https://api.api-futebol.com.br/v1/campeonatos", {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer test_ba8597de5edcf59eed7df197ce04d8"
+    async function getCampeonato() {
+      try {
+        const resposta = await fetch(
+          "https://api.api-futebol.com.br/v1/campeonatos",
+          {
+            method: "GET",
+            headers: {
+              Authorization: "Bearer live_8f52968d02ad7d589b472cc80dcf48"
+            }
+          }
+        );
+        const resultado = await resposta.json();
+        console.log(resultado);
+        setCampeonato(resultado);
+      } catch (error) {
+        console.error(error);
       }
-    })
-      .then((response) => {
-        console.log(response);
-        setCampeonato(response.json());
-      })
-      .catch((error) => console.log(error));
+    }
+    getCampeonato();
   }, []);
 
   return (
     <IonPage className={styles.homePage}>
       <IonHeader>
-        {/* <IonToolbar className="ion-no-margin ion-no-padding"> */}
-        {/* <IonImg src="/assets/login2.jpeg" /> */}
-        {/* </IonToolbar> */}
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton icon={arrowBack} text="" className="custom-back" />
+          </IonButtons>
+
+          <IonButtons slot="end">
+            <IonButton className="custom-button">
+              <IonIcon icon={shapesOutline} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+        <IonCardTitle className={`ion-text-center ${styles.heading}`}>
+          Lista de Campeonatos
+        </IonCardTitle>
       </IonHeader>
       <IonContent fullscreen>
-        <div className={styles.getStarted}>
-          <IonGrid>
-            <IonRow
-              className={`ion-text-center ion-justify-content-center ${styles.heading}`}
-            >
-              <IonCol size="11" className={styles.headingText}>
-                <IonCardTitle>
-                  Join millions of other people discovering their creative side
-                </IonCardTitle>
-              </IonCol>
-            </IonRow>
-            <>{campeonato.map((item) => alert(item))}</>
+        {campeonato.map((item) => (
+          <IonList key={item.campeonato_id}>
+            <IonToolbar>
+              <IonGrid className={styles.grid}>
+                <IonImg src={item.logo} style={{ height: "70px" }} />
+                <IonRow
+                  style={{ textAlign: "center", justifyContent: "center" }}
+                >
+                  <IonCol size="12">
+                    <IonCardTitle>{item.nome}</IonCardTitle>
 
-            <IonRow className={`ion-text-center ion-justify-content-center`}>
-              <IonRouterLink routerLink="/signup" className="custom-link">
-                <IonCol size="11">
-                  <IonButton
-                    className={`${styles.getStartedButton} custom-button`}
-                  >
-                    Get started &rarr;
-                  </IonButton>
-                </IonCol>
-              </IonRouterLink>
-            </IonRow>
-          </IonGrid>
-        </div>
+                    <p>{`Status: ${item.status ? item.status : "N/A"}`}</p>
+                    <p>{`Rodada atual: ${
+                      item.rodada_atual
+                        ? `${item.rodada_atual.nome} - ${item.rodada_atual.status} (${item.tipo})`
+                        : "N/A"
+                    }`}</p>
+                    <p>{`Fase atual: ${
+                      item.fase_atual.nome ? item.fase_atual.nome : "N/A"
+                    }`}</p>
+                    <p>{`Temporada: ${
+                      item.edicao_atual.temporada
+                        ? item.edicao_atual.temporada
+                        : "N/A"
+                    }`}</p>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonToolbar>
+          </IonList>
+        ))}
       </IonContent>
-
-      <IonFooter>
-        <IonGrid>
-          <Action
-            message="Already got an account?"
-            text="Login"
-            link="/login"
-          />
-        </IonGrid>
-      </IonFooter>
     </IonPage>
   );
 };
